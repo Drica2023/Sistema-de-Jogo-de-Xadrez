457e842d59a9951679d68517a34751c4c9eca6f5
Drica2023 <adrianamedeiros.dev@outlook.com>
2023-12-22 13:22:24 -0300

    Criado o Projeto e primeira classe Position

---
+++ chessSystem/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// testando a classe Position
+		
+		Position position = new Position (3 , 5);
+		
+		System.out.println(position);
+		
+	}
+
+}
